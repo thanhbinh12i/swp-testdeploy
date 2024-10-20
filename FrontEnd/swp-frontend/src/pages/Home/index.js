7d52a0949cac66235a15a49537346bafe9b72b43
thanhbinh12ii <nptbinh17092004@gmail.com>
2024-10-20 17:55:40 +0700

    commit

--- FrontEnd/swp-frontend/src/pages/Home/index.js
+++ FrontEnd/swp-frontend/src/pages/Home/index.js
@@ -1,19 +1,7 @@
-import { Row, Col, Card, Button, Carousel, Typography } from 'antd';
-import { DollarCircleOutlined, TeamOutlined, CarOutlined, GlobalOutlined, CreditCardOutlined, BankOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
-import './Home.scss';
-import banner1 from "../../assets/home/banner-1.jpg"
-import banner2 from "../../assets/home/banner-2.jpg"
-import banner3 from "../../assets/home/banner-3.jpg"
-import banner4 from "../../assets/home/banner-4.jpg"
-import deliver from "../../assets/home/deliver-home.jpg"
-import koiorder from "../../assets/home/koi-order.jpg"
-import koifarmtour from "../../assets/home/koi-farm-tour.jpg"
 
-const { Title, Paragraph } = Typography;
 function Home() {
       return (
-            <div className="home">
+            <div >
                 
             </div>
       );
